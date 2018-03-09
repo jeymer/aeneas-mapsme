0cc5fbbdfb2f20c225b5e21bdcd88afd5d004096
pl-aeneas <pl.aeneas.lang@gmail.com>
2018-03-08 20:31:27 -0500

    Edits.

--- android/run/old/offline-table-experiment/pull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in `seq 0 8`; do
-  for j in `seq 0 1`; do
-    for k in `seq 0 2`; do
-      adb pull /data/local/tmp/stoke/mapsme/run/offline-table-experiment/mapsme.$i.$j.$k.log 
-    done
-  done
-done
