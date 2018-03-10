84ca642c8928e9bac19dd28b50275c4ff168fba0
pl-aeneas <pl.aeneas.lang@gmail.com>
2018-03-09 22:14:50 -0500

    Data.

--- android/run/constrained2/synch-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-adb push run-constrained.sh /data/local/tmp/stoke/paper-bench/binary-mapsme/run
-adb shell /data/local/tmp/stoke/paper-bench/binary-mapsme/run/run-constrained.sh
--- android/run/old/offline-table-experiment/tmp/rename.sh
+++
@@ -1,5 +0,0 @@
-for i in `seq 0 5`; do
-  for j in `seq 1 2`; do
-    mv mapsme.$i.2.$j.log mapsme.$i.1.$j.log 
-  done
-done
